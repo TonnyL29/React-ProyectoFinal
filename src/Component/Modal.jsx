ab832ebaff3a3bfae0b0851e396e3a8b84bbdbe9
Tonny <82624339+TonnyL29@users.noreply.github.com>
2023-02-03 17:21:29 -0300

    Add component Modal

--- src/Component/Modal.jsx
+++ src/Component/Modal.jsx
@@ -1,7 +1,11 @@
-const Modal = () =>{
+const Modal = ({image, price, description}) =>{
     return (
         <div>
-            
+            <img class="img-fluid mx-auto" src={image} />
+			<p class="text-center">{description}</p>
+			<p class="text-center text-primary fs-3">Precio: ${price}</p>
+			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btnclose">Cerrar</button>
+			<button class="btn btn-primary sticky-bottom"><span class="material-symbols-outlined">add_shopping_cart</span></button>
         </div>
     )
 }
