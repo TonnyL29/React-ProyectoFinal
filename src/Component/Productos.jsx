e0c8499ca010c5eb330798cba6fa8ea630356af0
Tonny <82624339+TonnyL29@users.noreply.github.com>
2023-02-04 10:39:12 -0300

    Commit para la primer entrega

--- src/Component/Productos.jsx
+++ src/Component/Productos.jsx
@@ -1,41 +1,46 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState, useEffect} from "react";
 import './Resource/Productos.css';
 
 const Productos = ({product}) => {
-    const url = `https://fakestoreapi.com/${product}`;
-    const [Prod, ProdData] = useState();
-    const fetchApi = async () =>{
-        const respuesta = await fetch(url)
-        const respuestaJSON = await respuesta.json();
-        ProdData(respuestaJSON);
-    }
-    useEffect(() =>{
-        fetchApi()
-    }, [])
     return(
-        <div className ="conteiner mt-4" id="">
-            <div className ="row gap-2" id="contenedor">
-            
-        { !Prod ? <h3 className="text-Center">Cargando productos...</h3> :
-            Prod.map((Prod, index) => {
-                return <div className="mx-auto card cardConteiner">
-		        <img src={Prod.image} className="card-img-top mx-auto pt-4 imgCard" alt=""></img>
+        <div className="row gap-2" id="contenedor">
+            <div className='text-center m-3 h3'><p>{product}</p></div>
+            <div className="mx-auto card cardConteiner">
+		        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" className="card-img-top mx-auto pt-4 imgCard" alt="" />
 		        <div className="card-body">
-		        <h5 className="card-title">{Prod.title}</h5>
-		        <p className="card-text"><br></br></p>
-		        <h4 className="card-subtitle mb-2 text-primary text-center">Precio: ${Prod.price}</h4>
-		        <p className="card-subtitle mb-2 text-muted">{Prod.category}</p>
-		        <button className="btn btn-primary">Ver producto</button>
-		        <button className="btn btn-primary ms-2"><span className="material-symbols-outlined">
-		        add_shopping_cart</span></button>
+		            <h5 className="card-title">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h5>
+		            <p className="card-text"><br /></p>
+		            <h4 className="card-subtitle mb-2 text-primary text-center">Precio: $109.95</h4>
+		            <p className="card-subtitle mb-2 text-muted">men's clothing</p>
+		            <button onclick="VerProducto(1)" className="btn btn-primary">Ver producto</button>
+		            <button onclick="AddCar(1)" className="btn btn-primary"><span className="material-symbols-outlined">
+		            add_shopping_cart</span></button>
 		        </div>
 	        </div>
-            })
-        }
-
-            </div>
-        </div>
+            <div className="mx-auto card cardConteiner">
+		        <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" className="card-img-top mx-auto pt-4 imgCard" alt="" />
+		        <div className="card-body">
+		            <h5 className="card-title">Mens Casual Premium Slim Fit T-Shirts </h5>
+		            <p className="card-text"><br /></p>
+		            <h4 className="card-subtitle mb-2 text-primary text-center">Precio: $22.3</h4>
+		            <p className="card-subtitle mb-2 text-muted">men's clothing</p>
+		            <button onclick="VerProducto(2)" className="btn btn-primary">Ver producto</button>
+		            <button onclick="AddCar(2)" className="btn btn-primary"><span className="material-symbols-outlined">
+		            add_shopping_cart</span></button>
+		        </div>
+	        </div>
+        <div className="mx-auto card cardConteiner">
+		    <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" className="card-img-top mx-auto pt-4 imgCard" alt="" />
+		    <div className="card-body">
+		        <h5 className="card-title">Mens Cotton Jacket</h5>
+		        <p className="card-text"><br /></p>
+		        <h4 className="card-subtitle mb-2 text-primary text-center">Precio: $55.99</h4>
+		        <p className="card-subtitle mb-2 text-muted">men's clothing</p>
+		        <button onclick="VerProducto(3)" className="btn btn-primary">Ver producto</button>
+		        <button onclick="AddCar(3)" className="btn btn-primary"><span className="material-symbols-outlined">
+		        add_shopping_cart</span></button>
+		    </div>
+	    </div>
+    </div>
     )
 
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import NavBar from './Component/NavBar';
 import Productos from './Component/Productos';
 import Footer from './Component/Footer';
-import ContenedorModal from './Component/ContenedorModal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <div>
   <NavBar />
-  <Productos product = 'products/'/>
-  <ContenedorModal />
+  <Productos product = 'Bienvenidos a DeModa, tu lugar siempre de moda'/>
   <Footer />
 </div>
 );
