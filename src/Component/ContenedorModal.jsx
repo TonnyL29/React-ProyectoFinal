bb19450cac8b2c6f33cc0dfb0b45a076b7d69df5
Tonny <82624339+TonnyL29@users.noreply.github.com>
2023-02-04 11:21:15 -0300

    Modificacion del Modal
    Agregado del header, body y footer del Modal

--- src/Component/ContenedorModal.jsx
+++ src/Component/ContenedorModal.jsx
@@ -1,13 +1,15 @@
 import Modal from "./Modal";
 
-const ContenedorModal = () =>{
+const ContenedorModal = (state) =>{
     return (
-        <div class="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
+        <div className="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog">
                 <Modal />
             </div>
         </div>
     )
+    let showModal = document.getElementById('model');
+    showModal.add.className = {state};
 }
 
 export default ContenedorModal;
--- src/Component/Modal.jsx
+++ src/Component/Modal.jsx
@@ -1,11 +1,19 @@
-const Modal = ({image, price, description}) =>{
+const Modal = ({image, title, price, description}) =>{
     return (
-        <div>
-            <img class="img-fluid mx-auto" src={image} />
-			<p class="text-center">{description}</p>
-			<p class="text-center text-primary fs-3">Precio: ${price}</p>
-			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btnclose">Cerrar</button>
-			<button class="btn btn-primary sticky-bottom"><span class="material-symbols-outlined">add_shopping_cart</span></button>
+        <div className="modal-content">
+            <div className="modal-header">
+                <h1 className="modal-title fs-5" id="modal-title">{title}</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body" id="modal-body">
+                <img className="img-fluid mx-auto" src={image} alt='Imagen del producto' />
+		    	<p className="text-center">{description}</p>
+		    	<p className="text-center text-primary fs-3">Precio: ${price}</p>
+            </div>
+                <div className="modal-footer" id="modal-footer">
+		    	    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" id="btnclose">Cerrar</button>
+		    	    <button className="btn btn-primary sticky-bottom"><span className="material-symbols-outlined">add_shopping_cart</span></button>
+                </div>
         </div>
     )
 }
