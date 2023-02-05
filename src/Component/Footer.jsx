b23c44a73d9cb1681ffe6ff2d6987ae87f28f429
Tonny <82624339+TonnyL29@users.noreply.github.com>
2023-02-05 16:55:44 -0300

    Modificación Footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,11 +3,18 @@ import facebook from './Resource/image/Facebook.png';
 import instagram from './Resource/image/Instagram.png';
 import WhatsApp from './Resource/image/WhatsApp.png';
 import Youtube from './Resource/image/Youtube.png';
+import Productos from './Productos';
 
 const Footer = () => {
+
+    const cargarCat = (valor) =>{
+        <Productos product= {"products/category/" + valor} />
+        console.log(valor)
+    }
+
     return (
         <div className ="container-fluid w-auto text-center mt-5" >
-            <div className="row p-5 bg-secondary aling-item-center">
+            <div className="row ps-5 pe-5 bg-secondary aling-item-center">
                 <div className="col-sx-12 col-md-6 col-lg-3 text-white">
                     <div>
                     <a href="/"><img src={Logo} alt="Logo De Moda" width={120} /></a>
@@ -16,10 +23,10 @@ const Footer = () => {
                 </div>
                 <div className="col-sx-12 col-md-6 col-lg-3 aling-center">
                     <div><p className='h4 text-center text-white'>Categorias</p></div>
-                    <div className='text-center h6 mt-3'><a className='text-dark text-decoration-none' href='/'>Electronics</a></div>
-                    <div className='text-center h6 mt-3'><a className='text-dark text-decoration-none' href='/'>Jewelery</a></div>
-                    <div className='text-center h6 mt-3'><a className='text-dark text-decoration-none' href='/'>Men's clothing</a></div>
-                    <div className='text-center h6 mt-3'><a className='text-dark text-decoration-none' href='/'>Women's clothing</a></div>
+                    <div className='text-center h6 mt-3'><a onClick={cargarCat(`electronics`)} className='text-dark text-decoration-none' href='/'>Electronics</a></div>
+                    <div className='text-center h6 mt-3'><a onClick={cargarCat(`jewelery`)} className='text-dark text-decoration-none' href='/'>Jewelery</a></div>
+                    <div className='text-center h6 mt-3'><a onClick={cargarCat(`men's%20clothing`)} className='text-dark text-decoration-none' href='/'>Men's clothing</a></div>
+                    <div className='text-center h6 mt-3'><a onClick={cargarCat(`women's%20clothing`)} className='text-dark text-decoration-none' href='/'>Women's clothing</a></div>
                 </div>
                 <div className="col-sx-12 col-md-6 col-lg-3">
                 <div><p className='h4 text-center text-white'>Nosotros</p></div>
@@ -31,10 +38,10 @@ const Footer = () => {
                 </div>
                 <div className="col-sx-12 col-md-6 col-lg-3">
                     <div><p className='h4 text-center text-white'>Contactanos</p></div>
-                    <div className='text-center h6'><img className='me-2 mt-2' src={WhatsApp} alt="WhatsApp" srcset="" width={35}/> WhatsApp</div>
-                    <div className='text-center h6'><img className='me-2 mt-2' src={instagram} alt="Instagram" srcset="" width={35}/>Instagram</div>
-                    <div className='text-center h6'><img className='me-2 mt-2' src={facebook} alt="WhatsApp" srcset="" width={35}/>Facebook</div>
-                    <div className='text-center h6'><img className='me-2 mt-2' src={Youtube} alt="WhatsApp" srcset="" width={35}/>YouTube</div>
+                    <div className='text-center h6'><a href="/"><img className='me-2 mt-2' src={WhatsApp} alt="WhatsApp" srcset="" width={35}/> WhatsApp</a></div>
+                    <div className='text-center h6'><a href="/"><img className='me-2 mt-2' src={instagram} alt="Instagram" srcset="" width={35}/>Instagram</a></div>
+                    <div className='text-center h6'><a href="/"><img className='me-2 mt-2' src={facebook} alt="WhatsApp" srcset="" width={35}/>Facebook</a></div>
+                    <div className='text-center h6'><a href="/"><img className='me-2 mt-2' src={Youtube} alt="WhatsApp" srcset="" width={35}/>YouTube</a></div>
                 </div>
             </div>
         </div>
