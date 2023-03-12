93be751b3a197b8aabd1da05709fa66f6d265724
Tonny <82624339+TonnyL29@users.noreply.github.com>
2023-03-12 18:27:57 -0300

    Conexión Firebase

--- src/Component/Productos/ProducConteiner.jsx
+++ src/Component/Productos/ProducConteiner.jsx
@@ -1,17 +1,22 @@
-import { useParams } from 'react-router-dom';
 import './Productos.css';
 import ProducDetail from './ProducDetail'
-import useFetchHook from '../Resource/useFetchHook';
+import { useEffect, useState } from 'react';
+import { collection, getFirestore, getDocs } from 'firebase/firestore';
 
 const Productos = () => {
-    const {categoryId} = useParams();
-    let value = categoryId;
-    let prod = 'P';
-    (!value) ? prod = 'P': prod = 'SC';
-    let Prod = useFetchHook([prod, value]);
+     const [items, setItems] = useState([]);
+
+ useEffect(() =>{
+    const db = getFirestore(); 
+    const itemCollection = collection(db, "item");
+     getDocs(itemCollection).then(Prod => {
+         setItems(Prod.docs.map(element => ({id:element.id, ...element.data()})))
+     })
+}, []);
+
 return(
         <div className ='conteiner mt-4' id=''>
-            <ProducDetail Prod={Prod}/>
+            <ProducDetail Prod={items}/>
        </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjWU3XM0cJB3vYaxbEGDkxiMkX7foT1io",
+  authDomain: "demoda-bb639.firebaseapp.com",
+  projectId: "demoda-bb639",
+  storageBucket: "demoda-bb639.appspot.com",
+  messagingSenderId: "956283637349",
+  appId: "1:956283637349:web:2225951f1a077e7321808c"
+};
+
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
